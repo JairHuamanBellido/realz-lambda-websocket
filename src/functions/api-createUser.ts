37fffd61ac6cbaea5857496bab14f228ff304d29
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2023-04-14 21:28:12 -0500

    feat: add log error on request

--- src/functions/api-createUser.ts
+++ src/functions/api-createUser.ts
@@ -22,6 +22,7 @@ export const handler = async (event: APIGatewayEvent) => {
       body: isValidBody,
     };
   } catch (error) {
+    console.log(error);
     return {
       statusCode: 500,
       body: error,
